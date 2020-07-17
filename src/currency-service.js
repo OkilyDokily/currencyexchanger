579452b24ac0d8bc32ea2ea3c6e892ac828ca43f
me <me@me.me.com>
2020-07-17 15:46:39 -0700

    final state for anycurrency

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,12 +1,12 @@
 export class CurrencyService{
  
   async getExchangeForCountry(exchange, initial, amount){
+    console.log(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${initial}`)
     try{
       var exchangeRates = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${initial}`);
     }
     catch(err)
     { //provide a somewhat more helpful fetch error than "failed to fetch"
-      console.dir(err);
       throw Error("You made a request to a site that does not exist or is down. Or your OWN network is down.");
     }
     
@@ -17,18 +17,23 @@ export class CurrencyService{
         throw Error("You aren't using a valid API key.")
       }
 
+      if(jsonified["error-type"] === "base-code-only-on-pro"){
+        throw Error("You can only find exchange rates for the USA and the Euro at your access level.")
+      }
+
       if(jsonified["unsupported-code"] === "invalid-key"){
         throw Error("You aren't using a supported country code.")
       }
+
       
       if(jsonified.result === "error"){//this error comes from the exchangerate-api site
-        throw Error ("There was something wrong with you query. You may be making a query that is ill-formed/not compatible with this applications access level.");
+        throw Error ("There was something wrong with your query: " + jsonified["error-type"]);
       }
       let conversionRate = jsonified["conversion_rates"][exchange];
       return conversionRate * amount; 
     }
     else{
-      throw Error("Error code: " + exchangeRates.status);
+      throw Error("HTTP Error code: " + exchangeRates.status);
     }
   
   }
