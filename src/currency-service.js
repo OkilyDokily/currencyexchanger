ee72898b42886ea68751ee547a885a060a8aa70e
me <me@me.me.com>
2020-07-20 15:05:19 -0700

    finished prepopulated menu

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,7 +1,26 @@
 export class CurrencyService{
  
+  async prePopulated(){
+    if (sessionStorage.getItem("countries")){
+      var obj = JSON.parse(sessionStorage.countries);
+      return obj;
+    } 
+
+    try{
+      var exchangeRates = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+    }
+    
+    catch(err)
+    { //provide a somewhat more helpful fetch error than "failed to fetch"
+      throw Error("You made a request to a site that does not exist or is down. Or your OWN network is down.");
+    }
+    let jsonified = await exchangeRates.json();
+    sessionStorage.setItem('countries', JSON.stringify(jsonified["conversion_rates"]));
+    
+    return jsonified["conversion_rates"];
+  }
+
   async getExchangeForCountry(exchange, initial, amount){
-    console.log(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${initial}`)
     try{
       var exchangeRates = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${initial}`);
     }
@@ -12,7 +31,7 @@ export class CurrencyService{
     
     if(exchangeRates.status === 200){
       let jsonified = await exchangeRates.json();
-      console.log(jsonified);
+
       if(jsonified["error-type"] === "invalid-key"){
         throw Error("You aren't using a valid API key.")
       }
@@ -25,12 +44,13 @@ export class CurrencyService{
         throw Error("You aren't using a supported country code.")
       }
 
-      
       if(jsonified.result === "error"){//this error comes from the exchangerate-api site
         throw Error ("There was something wrong with your query: " + jsonified["error-type"]);
       }
       let conversionRate = jsonified["conversion_rates"][exchange];
       return conversionRate * amount; 
+      let usd =  amount / result[initial];
+      let conversion = usd * result[exchange]; 
     }
     else{
       throw Error("HTTP Error code: " + exchangeRates.status);
--- src/main.js
+++ src/main.js
@@ -5,11 +5,46 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+
 $(document).ready(function(){
-  $("button").click(function(){
+  (async () => {
+    let currencyService = new CurrencyService();
+    let result = await currencyService.prePopulated();
+    let countries = Object.keys(result);
+    let text = "";
+    countries.forEach(countryCode =>{
+      text+= `<option value="${countryCode}">${countryCode}</option>`;
+    });
+    $("#initial2").html(text);
+    $("#exchange2").html(text);    
+    
+  
+    $("#button2").click(function(){
+      let exchange = $("#exchange2 option:selected").val();
+      let initial = $("#initial2 option:selected").val();
+      let amount = $("#amount2").val();
+
+      let usd =  amount / result[initial];
+      let conversion = usd * result[exchange]; 
+      
+      var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: exchange,
+      });
+      let formattedResult = formatter.format(conversion);
+      $("#results2").html(formattedResult);
+    });
+  })();
+});
+
+
+$(document).ready(function(){
+
+ 
+  $("#button").click(function(){
     let exchange = $("#exchange option:selected").val();
     let initial = $("#initial option:selected").val();
-    console.log(initial)
+    
     let amount = $("#amount").val();
     let currencyService = new CurrencyService();
     currencyService.getExchangeForCountry(exchange,initial, amount).then((result)=>{
